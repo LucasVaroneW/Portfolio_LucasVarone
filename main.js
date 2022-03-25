9e5410ec9dc62b41c927f574551ea2323b2d723f
LucasVaroneW <81322848+LucasVaroneW@users.noreply.github.com>
2022-03-25 12:15:50 -0300

    scroll
    scroll #home

--- main.js
+++ main.js
@@ -2,6 +2,13 @@
 $("a[href='#ejemploCarrusel']").click(function() {
 $("html, body").animate({ scrollTop: $("#ejemploCarrusel").offset().top });
 });
+
+$("a[href='#home']").click(function() {
+    $("html, body").animate({ scrollTop: $("#home").offset().top });
+    });
+
+
+
 ScrollReveal().reveal(".navbar", { delay: 250 });
 ScrollReveal().reveal(".home__title--primary", { delay: 550 });
 ScrollReveal().reveal(".home__title", { delay: 650 });
