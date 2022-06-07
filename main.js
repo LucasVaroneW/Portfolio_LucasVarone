53d935d1b93be0c323c4d4878d77171a01279363
LucasVaroneW <81322848+LucasVaroneW@users.noreply.github.com>
2022-06-07 12:10:40 -0300

    terminado-sin-form
    form comentado, falta desarrollar la base de datos

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-
 $("a[href='#ejemploCarrusel']").click(function() {
-$("html, body").animate({ scrollTop: $("#ejemploCarrusel").offset().top });
-});
-
-$("a[href='#home']").click(function() {
-    $("html, body").animate({ scrollTop: $("#home").offset().top });
+    $("html, body").animate({ scrollTop: $("#ejemploCarrusel").offset().top });
     });
-
-
-ScrollReveal().reveal(".navbar", { delay: 250 });
-ScrollReveal().reveal(".home__title--primary", { delay: 550 });
-ScrollReveal().reveal(".home__title", { delay: 650 });
-ScrollReveal().reveal(".home__title--secondary", { delay: 750 });
-ScrollReveal().reveal(".flicker", { delay: 950 });
-
+    
+    $("a[href='#home']").click(function() {
+        $("html, body").animate({ scrollTop: $("#home").offset().top });
+        });
+    
+    
+    ScrollReveal().reveal(".navbar", { delay: 250 });
+    ScrollReveal().reveal(".home__title--primary", { delay: 550 });
+    ScrollReveal().reveal(".home__title", { delay: 650 });
+    ScrollReveal().reveal(".home__title--secondary", { delay: 750 });
+    ScrollReveal().reveal(".flicker", { delay: 950 });
+    
+    
