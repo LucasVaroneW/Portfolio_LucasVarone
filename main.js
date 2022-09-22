d528bcf6cd311b548c96d0ba2fcfb4fc85bf3be1
LucasVaroneW <81322848+LucasVaroneW@users.noreply.github.com>
2022-09-22 17:03:35 -0300

    repair-scroll
    2

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
-$("a[href='#ejemploCarrusel']").click(function() {
-    $("html, body").animate({ scrollTop: $("#ejemploCarrusel").offset().top });
+function downClick(){
+    $('html, body').animate({scrollTop: $('#ejemploCarrusel').offset().top})
+}
+    
+
+$("a[href='#home']").click(function() {
+    $("html, body").animate({ scrollTop: $("#home").offset().top });
     });
-    
-    $("a[href='#home']").click(function() {
-        $("html, body").animate({ scrollTop: $("#home").offset().top });
-        });
-    
-    
-    ScrollReveal().reveal(".navbar", { delay: 250 });
-    ScrollReveal().reveal(".home__title--primary", { delay: 550 });
-    ScrollReveal().reveal(".home__title", { delay: 650 });
-    ScrollReveal().reveal(".home__title--secondary", { delay: 750 });
-    ScrollReveal().reveal(".flicker", { delay: 950 });
+
+
+ScrollReveal().reveal(".navbar", { delay: 250 });
+ScrollReveal().reveal(".home__title--primary", { delay: 550 });
+ScrollReveal().reveal(".home__title", { delay: 650 });
+ScrollReveal().reveal(".home__title--secondary", { delay: 750 });
+ScrollReveal().reveal(".flicker", { delay: 950 });
     
     
